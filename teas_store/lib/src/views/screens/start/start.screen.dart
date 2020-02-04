994b62ba3e79eec6f925ad13e8adb767924fb4fe
MichaelThai <huy.thai@ymail.com>
2020-02-04 22:46:57 +0700

    implement background image widget

--- teas_store/lib/src/views/screens/start/start.screen.dart
+++ teas_store/lib/src/views/screens/start/start.screen.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:teas_store/src/routes/route.dart';
-import 'package:teas_store/src/views/screens/login/login.screen.dart';
+import 'package:teas_store/src/shared/widgets/background_image.widget.dart';
 
 class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Start Screen'),
-      ),
-      body: Center(
-        child: InkWell(
-          onTap: () {
-            navigateTo(context, LoginScreen());
-          },
-          child: Container(
-            child: Text('go to sign in'),
+      body: Stack(
+        children: <Widget>[
+          BackgroundImage(
+            imgUrl: 'assets/images/wall_13.jpg',
+            colorFilter: ColorFilter.mode(
+                Colors.black54.withOpacity(0.3), BlendMode.darken),
           ),
-        ),
+        ],
       ),
     );
   }

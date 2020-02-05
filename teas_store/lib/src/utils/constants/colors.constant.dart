e13f7ae049fbb0a8a9bb14089550603bd64dec44
Michael Thai <>
2020-02-05 17:53:52 +0700

    finish start screen

--- teas_store/lib/src/utils/constants/colors.constant.dart
+++ teas_store/lib/src/utils/constants/colors.constant.dart
@@ -1,9 +1,14 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 class ColorConstant {
   static const Color DEFAULT = Color(0xff99332E);
   static const Color DEFAULT_LIGHT = Color(0xffbf403a);
   static const Color DEFAULT_DARK = Color(0xff7a2925);
-
+  static const Color GREEN_COLOR = Color(0xff0d9f67);
   static const Color SMOKE = Color(0xfff3f3f3);
+
+  static const ColorFilter DARK_FILTER =
+      ColorFilter.mode(Colors.black54, BlendMode.dst);
 }
--- teas_store/lib/src/views/screens/start/start.screen.dart
+++ teas_store/lib/src/views/screens/start/start.screen.dart
@@ -1,16 +1,54 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:teas_store/src/shared/widgets/background_image.widget.dart';
+import 'package:teas_store/src/shared/widgets/rounded_button.widget.dart';
+import 'package:teas_store/src/shared/widgets/rounded_outline_button.widget.dart';
+import 'package:teas_store/src/utils/constants/colors.constant.dart';
+import 'package:teas_store/src/utils/constants/images.constant.dart';
 
 class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
+        fit: StackFit.expand,
+        overflow: Overflow.visible,
         children: <Widget>[
           BackgroundImage(
-            imgUrl: 'assets/images/wall_13.jpg',
-            colorFilter: ColorFilter.mode(
-                Colors.black54.withOpacity(0.3), BlendMode.darken),
+            imgUrl: ImageConstant.START_SCREEN_BACKGROUND,
+            colorFilter: ColorConstant.DARK_FILTER,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 80.0,
+                  child: Image.asset(ImageConstant.LOGO),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    RoundedButton(
+                      buttonLabel: 'Log In',
+                      buttonLabelStyle: TextStyle(color: Colors.white),
+                      buttonColor: ColorConstant.DEFAULT,
+                      onPress: () {},
+                    ),
+                    RoundedOutlineButton(
+                      buttonLabel: 'Sign Up',
+                      buttonLabelStyle: TextStyle(color: ColorConstant.DEFAULT),
+                      onPress: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ],
       ),
